134a520c6dbf315c4a0fbb7fc250919bc0277ae1
Lerpardo <ls6182315@gmail.com>
2022-12-13 08:37:08 -0300

    add next-image app preview

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,14 @@ interface HomeProps {
   count: number,
 }
 
+import Image from "next/image";
+import appPreview from '../assets/aplicacao-trilha-ignite.png'
 export default function Home(props: HomeProps) {
   return (
     <>
       <h1>Hello TypeScript</h1>
       <p>teste</p>
+      <Image src={appPreview} alt='appPreview' quality={100}/>
       <h3>{props.count}</h3>
     </>
   );
