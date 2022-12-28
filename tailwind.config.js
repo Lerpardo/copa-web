d4f4abc6a3fe9da23d6f974697cd627708adce9d
Lerpardo <ls6182315@gmail.com>
2022-12-28 00:15:23 -0300

    add css config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,17 +5,30 @@ module.exports = {
   ],
   theme: {
     extend: {
-      fontFamily:{
+      fontFamily: {
         sans: 'Roboto, sans-serif'
       }
     },
-
+    backgroundImage:{
+      app: 'url(/web-bg.png)'
+    },
     colors: {
       ignite: {
         500: '#129E57'
       },
-      gray:{
-        900:'#121214'
+      yellow: {
+        500: '#F7DD43'
+      },
+      white: {
+        500: '#FFFFFF',
+        700: '#E5CD3D',
+      },
+      gray: {
+        100: '#E1E1E6',
+        300: '#8D8D99',
+        600: '#323238',
+        800: '#202024',
+        900: '#121214',
       }
     }
   },
